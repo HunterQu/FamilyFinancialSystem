fe41ad83461d865753773b013d6c62d3bfeaa65f
HunterQu <1762011587@qq.com>
2024-08-28 10:04:17 +0800

    实现主页框架

--- accounting/views.py
+++ accounting/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def index(request):
-    return  render(request, 'accounting/index.html')
+    all_accounts = Account.objects.all()
+    sub_categories = SubCategory.objects.all()
+    context = {
+        'accounts': all_accounts,
+        'sub_categories': sub_categories
+    }
+    print(context)
+    return  render(request, 'accounting/index.html', context)
 def login(request):
     return  render(request, 'accounting/login.html')
