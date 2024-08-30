c88aec0b3033044433e015a0d24a2ce5ab312c73
HunterQu <1762011587@qq.com>
2024-08-30 09:49:02 +0800

    实现记账功能（有bug）

--- accounting/urls.py
+++ accounting/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', register, name='register'),
     path('retrieve_category/', views.retrieve_category, name='retrieve_category'),
+    path('retrieve_subcategory/', views.retrieve_subcategory, name='retrieve_subcategory'),
+    path('record_income_expense/', views.record_income_expense, name='record_income_expense'),
 ]
